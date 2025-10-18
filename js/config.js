00f798f0f5d21935b0cf04d2e7442d40af656d8d
Osenat Olusola Alonge <131201117+etaoko333@users.noreply.github.com>
2025-10-18 14:50:49 +0100

    Update AWS Cognito configuration
    Updated AWS Cognito configuration with new User Pool ID and Client ID.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_c4F61ushZ', // Your User Pool ID
-        userPoolClientId: '6haset79jln1n5jrejqu1l8ibi', // Your User Pool Client ID
+        userPoolId: 'us-east-1_y2bDcSgDW', // Your User Pool ID
+        userPoolClientId: '5554qi54le1jqsg738cetiv8af', // Your User Pool Client ID
         region: 'us-east-1' // Your AWS region
     }
 };
@@ -9,3 +9,4 @@ window._config = {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
